e8b81e5386449b22d5dc6c778d1f60529e31383a
Radek Machan <70884019+radekmachan@users.noreply.github.com>
2023-02-16 22:38:22 +0800

    Add files via upload

--- Elements.js
+++ Elements.js
@@ -25,11 +25,11 @@ class Header extends HTMLElement {
             target="_blank">Booking System</a> <a href="Facilities.html#MicroFluor">Microscope
             Selector</a><a href="FIJI/FIJIntro.html" target="_blank">Introduction
             to FIJI</a><a href="MicroscopyCourse/MicroCourse22.html" target="_blank">Microscopy
-            Course</a><a href="MicroTalks.html" id = "uTalk">μ-Talks</a></div>
+            Course</a><a href="MicroTalks.html" id = "uTalk">μ-Talks</a><a href="ImContest21.html" id = "ImCont">Image Contest</a></div>
       </div>
       <div class="dropdown"> <button class="dropbtn"><a href="Team.html" id = "Team">Our
             Team</a></button></div>
-      <div class="dropdown"> <button class="dropbtn"><a href="index.html#About">About
+      <div class="dropdown"> <button class="dropbtn"><a href="index.html#About" id = "About">About
             Us</a></button>
         <div class="dropdown-content"> <a href="Lab.html" id = "Lab">NOBIC Laboratory</a>
           <a href="Lab.html#workshop">Prototyping Workshop</a><a href="https://www.youtube.com/channel/UCwka5W-n2mfAqLUz8L5eorA"
@@ -45,18 +45,76 @@ class Header extends HTMLElement {
 }class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `    
-      <footer>            
-        <div class="footer"> <a href="https://www.ntu.edu.sg/medicine" target="_blank">LKCMedicine</a>
+     <footer>             
+        <div class = "footer"> <a href="https://www.ntu.edu.sg/medicine" target="_blank">LKCMedicine</a>
         | <a href="http://www.scelse.sg/Home" target="_blank">SCELSE</a> | <a
 
           href="https://www.ntu.edu.sg/Pages/home.aspx" target="_blank">NTU</a>
-        <p>© NOBIC, last modified: <span id="datemod"></span></p>
-      </div>         
-      </footer>     
+		  <p> © NOBIC, last modified: <span id="datemod"></span></p>
+     
+      </div> 
+</footer>	  
+           
+    `;
+  }
+}class Footer2 extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `    
+     <footer>             
+        <div class = "footer"> <a href="https://www.ntu.edu.sg/medicine" target="_blank">LKCMedicine</a>
+        | <a href="http://www.scelse.sg/Home" target="_blank">SCELSE</a> | <a
+
+          href="https://www.ntu.edu.sg/Pages/home.aspx" target="_blank">NTU</a> | <a
+
+          href="https://www.singascope.sg/" target="_blank">SingaScope</a>
+		  <p> © <a href="https://www.nobic.sg">NOBIC</a></p>
+     
+      </div> 
+</footer>	  
+           
+    `;
+  }
+}class Footer3 extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `    
+     <footer>             
+        <div class = "footer"> <a href="ABIF.html">ABIF</a>
+        | <a href="http://www.scelse.sg/Home" target="_blank">SCELSE</a> | <a
+
+          href="https://www.ntu.edu.sg/Pages/home.aspx" target="_blank">NTU</a> | <a
+
+          href="https://www.singascope.sg/" target="_blank">SingaScope</a>
+		  <p> © <a href="https://www.nobic.sg">NOBIC</a></p>
+     
+      </div> 
+</footer>	  
+           
+    `;
+  }
+}class Footer4 extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `    
+     <footer>             
+        <div class = "footer"> <a href="AOBIP.html" >AOBIP</a> | 
+		<a href="https://www.ntu.edu.sg/medicine" target="_blank">LKCMedicine</a>
+        | <a
+
+          href="https://www.ntu.edu.sg/Pages/home.aspx" target="_blank">NTU</a> | <a
+
+          href="https://www.singascope.sg/" target="_blank">SingaScope</a>
+		  <p> © <a href="https://www.nobic.sg">NOBIC</a></p>
+     
+      </div> 
+</footer>	  
+           
     `;
   }
 }
 
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
+customElements.define('sub-footer', Footer2);
+customElements.define('abif-footer', Footer3);
+customElements.define('aobip-footer', Footer4);
+
 
